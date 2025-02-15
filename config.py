2b7f714b056da4154c744e67e086b60ef8fcc481
surajit77707 <surajitsingh77707@gmail.com>
2025-02-15 12:32:24 -0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_USERNAME = getenv("BOT_USERNAME")
-OWNER_ID = int(getenv(OWNER_ID""))
+OWNER_ID = int(getenv("OWNER_ID"))
 LOGGER_ID = int(getenv("LOGGER_ID"))
 MONGO_URL = getenv("MONGO_DB_URI")
 
