b9e9450460a270d4ad828a7eaa71f2cfe12f3d96
surajit77707 <surajitsingh77707@gmail.com>
2025-02-18 01:51:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = 25657194
-API_HASH = "cee5bdd9803dc1fbac282863204bdfec"
-BOT_TOKEN = "7892423173:AAFLR_hwGjpFjIdV39d9rxEcOyul9JwYpI0"
-BOT_USERNAME = "Venomcbot"
-OWNER_ID = 7900262575
+API_ID = 23967991
+API_HASH = "a2c3ccfaff4c2dbbff7d54981828d4f1"
+BOT_TOKEN = "7848626329:AAFE8i9wj4d1ostJFk9Msy5YPVcVk_OlJps"
+BOT_USERNAME = "Protectorvbot"
+OWNER_ID = 6375272628
 LOGGER_ID = -1002107679944
 MONGO_URL = "mongodb+srv://surajit69697:surajit69697@cluster0.7rwzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-SUDOERS = filters.user([7900262575,1883889098,7638575366])
+SUDOERS = filters.user([1883889098,7638575366])
