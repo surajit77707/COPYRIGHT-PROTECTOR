80b044d88bfb5ba134d062ec59f1d9187d28edeb
surajit77707 <surajitsingh77707@gmail.com>
2025-02-26 13:05:16 -0800

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_ID = 23967991
 API_HASH = "a2c3ccfaff4c2dbbff7d54981828d4f1"
 BOT_TOKEN = "7848626329:AAFE8i9wj4d1ostJFk9Msy5YPVcVk_OlJps"
 BOT_USERNAME = "Protectorvbot"
-OWNER_ID = 6375272628
+OWNER_ID = 7921906677
 LOGGER_ID = -1002107679944
 MONGO_URI = "mongodb+srv://xmen7151:xmen7151@cluster0.4h4js.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
